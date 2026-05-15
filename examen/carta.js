f7e9fde160d139ea4fa7843c4c1c1ced8e6d1004
Tobias <tobiasschmidt711@gmail.com>
2026-05-15 19:33:54 -0300

    trabajado en la logica de carta.js

---
+++ examen/carta.js
@@ -0,0 +1,68 @@
+class Carta {
+
+  code;
+  value;
+  suit;
+  imagen;
+
+    constructor(code, value, suit, imagen) {
+    this.code = code;      
+    this.value = value;    
+    this.suit = suit;      // tuve que buscar que era esto, es el palo de la carta, para mi futuro yo
+    this.imagen = imagen;  
+  }
+
+    toString() {
+    return JSON.stringify({
+      code: this.code,
+      value: this.value,
+      suit: this.suit,
+      imagen: this.imagen
+    });
+  }
+
+    static createFromJsonString(jsonString) {
+    const datos = JSON.parse(jsonString);
+    return new Carta(
+      datos.code,
+      datos.value,
+      datos.suit,
+      datos.imagen
+    );
+  }
+
+    createHtmlElement() {
+    const div = document.createElement('div');
+    div.className = 'tarjeta-carta';
+
+    const codeElem = document.createElement('h3'); //para mostrar el code de la carta, ejemplo h3 es hearts 3 (en ingles se entiende mejor)
+    codeElem.textContent = this.code;
+
+        const img = document.createElement('img');
+    img.src = this.imagen;
+    img.alt = this.code;
+    img.style.cursor = 'pointer';
+    img.addEventListener('click', () => {
+      window.open(this.imagen, '_blank'); // te abre la img en otra pestaña, el _blank es para que no te saque de la pagina
+    });
+
+    const infoElem = document.createElement('p');
+    infoElem.textContent = `${this.value} of ${this.suit}`; //completa el nombre de la carta, ejemplo 3 of hearts, lo hago en ingles porque queda mejor
+    
+    const btnGuardar = document.createElement('button');
+    btnGuardar.textContent = 'Guardar';
+    btnGuardar.className = 'btn-guardar';
+    btnGuardar.addEventListener('click', () => {
+      Carta.guardarCarta(this);
+      alert(`Carta "${this.code}" guardada`);
+    });
+    
+    div.appendChild(codeElem);
+    div.appendChild(img);
+    div.appendChild(infoElem);
+    div.appendChild(btnGuardar);
+    
+    return div;
+  }
+
+}
